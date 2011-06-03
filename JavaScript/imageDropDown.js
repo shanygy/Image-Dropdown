c274d3d2abb20bb40b7cca0a6b62de6056de47a1
Shany Lev <shany.gariby@gmail.com>
2011-06-04 02:53:55 +0300

    Register imageDropDown object in jQuery namespace

--- JavaScript/imageDropDown.js
+++ JavaScript/imageDropDown.js
@@ -1,34 +1,36 @@
-var imageDropDown = (function() {
-    var items = {};
-    
-    var set = function(key, value) {
-        items[key] = value;
-        return value;
-    };
-    
-    var get = function(key) {
-        return items[key];
-    };
-    
-    var remove = function(key) {
-        return delete items[key];
-    };
-    
-    var clear = function() {
-        for (key in items) {
-            delete items[key];
-        }
-    };
-    
-    var getItems = function(){
-        return items;
-    };
-    
-    return {
-        set: set,
-        get: get,
-        remove: remove,
-        clear: clear,
-        items: getItems
-    }    
-}());
+(function($) {
+	$.imageDropDown = (function() {
+		var items = {};
+		
+		var set = function(key, value) {
+			items[key] = value;
+			return value;
+		};
+		
+		var get = function(key) {
+			return items[key];
+		};
+		
+		var remove = function(key) {
+			return delete items[key];
+		};
+		
+		var clear = function() {
+			for (key in items) {
+				delete items[key];
+			}
+		};
+		
+		var getItems = function(){
+			return items;
+		};
+		
+		return {
+			set: set,
+			get: get,
+			remove: remove,
+			clear: clear,
+			items: getItems
+		}    
+	}());
+}(jQuery)();
